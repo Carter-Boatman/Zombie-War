cc61651a74865e62b98e5873645263ba1ebaae81
Carter-Boatman <carter.boatman@gmail.com>
2025-08-15 17:41:23 -0500

    Took the weapon list (now called weaponPool) and moved it into the WeaponCache.java file, removing the need for WeaponFactory.java. I renamed a couple references to said weapon list but everything else remains unchanged.

--- WeaponCache.java
+++ WeaponCache.java
@@ -10,14 +10,24 @@ public class WeaponCache {
     // Random instance used for shuffling and drawing weapons
     private final Random rand = new Random();
     // All available weapon types that can appear in caches
-    private final List<Weapon> available = WeaponFactory.defaultWeaponPool();
+    public static List<Weapon> weaponPool() {
+        List<Weapon> pool = new ArrayList<>();
+        pool.add(new Weapon("Shotgun", 90, 60));
+        pool.add(new Weapon("Submachine Gun", 70, 75));
+        pool.add(new Weapon("Assault Rifle", 80, 70));
+        pool.add(new Weapon("Pistol", 50, 85));
+        pool.add(new Weapon("Axe", 65, 80));
+        pool.add(new Weapon("Crowbar", 55, 78));
+        pool.add(new Weapon("Frying Pan", 40, 90));
+        return pool;
+    }
 
     // Generates a random cache list with a size between min and max inclusive
     public List<Weapon> generateCache(int min, int max) {
         int size = min + rand.nextInt((max - min) + 1);
         List<Weapon> cache = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            Weapon type = available.get(rand.nextInt(available.size()));
+            Weapon type = weaponPool().get(rand.nextInt(weaponPool().size()));
             cache.add(new Weapon(type.getName(), type.getDamage(), type.getAccuracyPercent()));
         }
         Collections.shuffle(cache, rand);
