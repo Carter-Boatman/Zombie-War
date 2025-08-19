59f240489742f1260ff3607a75a6917a6bcecc2d
Yeesaurus1 <148891216+Yeesaurus1@users.noreply.github.com>
2025-08-18 22:06:07 -0500

    Jonas's Zombie mutation stats tweak
    Lowered Tank attack from 30 to 21

--- Tank.java
+++ Tank.java
@@ -2,5 +2,6 @@ package zombiewar;
 
 // Tank zombie with very high health and strong attack
 public class Tank extends Zombie {
-    public Tank(String name, Mutation mutation) { super(name, 150, 30, mutation); }// High health and attack
+    public Tank(String name, Mutation mutation) { super(name, 150, 21, mutation); }// High health and attack
 }
+
