a697c41883b41cd655a50820c7c74a0b54ec6c35
Yeesaurus1 <148891216+Yeesaurus1@users.noreply.github.com>
2025-08-18 22:04:13 -0500

    Jonas's Zombie mutation stats tweak
    Look at added comment below object

--- CommonInfected.java
+++ CommonInfected.java
@@ -2,5 +2,7 @@ package zombiewar;
 
 // Common infected zombie with weak stats
 public class CommonInfected extends Zombie {
-    public CommonInfected(String name, Mutation mutation) { super(name, 30, 20, mutation); } // Weak health and attack
+    public CommonInfected(String name, Mutation mutation) { super(name, 30, 6, mutation); } // Weak health and attack
+    //lowered attack to 6 in line with suggestions
 }
+
